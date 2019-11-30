95922e9997f078a165e5cbb32002d3ebc6ec13ef
1991jing <379207614@qq.com>
2019-11-30 17:41:34 +0800

    实现多页面测试

---
+++ main.py
@@ -0,0 +1,8 @@
+import  jxc_lightApp
+import os,sys
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+PathProject = os.path.split(rootPath)[0]
+sys.path.append(rootPath)
+sys.path.append(PathProject)
